018ae230b4405c489646ee0664de3ce1b9441f7c
exequiel <eaguirre@example.com>
2018-08-10 13:06:03 -0300

    now in 3D

--- src/ccm/animation.py
+++ src/ccm/animation.py
@@ -1,7 +1,8 @@
 #coding: utf-8
-import scipy.integrate as spi
 import numpy as np
+import scipy.integrate as spi
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.animation as animation
 
 
@@ -20,18 +21,45 @@ def solve():
     return spi.odeint(diff_eqs,initial_condition,time_range),time_range
 
 
+def getCurves2D(fig,XYZ,X_M):
+    ax = plt.axes()
+    curve_M, = ax.plot(XYZ[:,0], XYZ[:,1])
+    curve_X_M, = ax.plot(X_M[:,0], X_M[:,1])
+    return curve_M,curve_X_M
+
+def getCurves3D(fig,XYZ,X_M):
+    ax=Axes3D(fig)
+    curve_M, = ax.plot(XYZ[:,0], XYZ[:,1],XYZ[:,2])
+    curve_X_M, = ax.plot(X_M[:,0], X_M[:,1],X_M[:,2])
+    return curve_M,curve_X_M
+
+mode='3D'
+
 XYZ,time_range=solve()
 #Animation code based on https://matplotlib.org/examples/animation/simple_anim.html
-fig, ax = plt.subplots()
-curve_M, = ax.plot(XYZ[:,0], XYZ[:,1])
 tau=5
 X_M=np.array([ [XYZ[i-2*tau,0],XYZ[i,0],XYZ[i-tau,0]] for i in range(0,len(time_range)) ])
-curve_X_M, = ax.plot(X_M[:,0], X_M[:,1])
+fig = plt.figure()
+if(mode=='2D'):
+    getCurves=getCurves2D
+else:
+    getCurves=getCurves3D
 
-def animate(i):
+curve_M,curve_X_M=getCurves(fig,XYZ,X_M)
+
+def animate2D(i):
     curve_M.set_data(XYZ[:i,0],XYZ[:i,1])  # update the data
     curve_X_M.set_data(X_M[:i,0],X_M[:i,1])  # update the data
 
+def animate3D(i):
+    animate2D(i)
+    curve_M.set_3d_properties(XYZ[:i,2])
+    curve_X_M.set_3d_properties(X_M[:i,2])
+
+if(mode=='2D'):
+    animate=animate2D
+else:
+    animate=animate3D
 
 ani = animation.FuncAnimation(fig, animate, len(time_range), interval=1)
 
