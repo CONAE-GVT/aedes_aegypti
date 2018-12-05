158f8e6c100227725d975f87ef914ae1488e17e8
exequiel.aguirre <exequiel.aguirre@gmail.com>
2018-12-05 20:58:11 -0300

    Now the steps and the initial time_range partition are independent. This will be usefull when we have a ram limitation.(spatial). We could have a time range with separation of days, but a time step of 1/20 days

--- src/rk.py
+++ src/rk.py
@@ -2,26 +2,30 @@ import numpy as np
 import pylab as pl
 import math
 
-def solve(_dYdt,Y0,time_range,args=()):
-	#main
-	Y=np.zeros([len(time_range),len(Y0)])
-	Y[0]=Y0#<---initial conditions
-	def dYdt(Y,t,_args):
-		Y[Y<0]=0#this is to make rk work
-		return np.array(_dYdt(Y,t,*_args))#decorate the function to return an np array
+def solve(_dYdt,Y0,time_range,args=(),steps=1):
+    #main
+    Y=np.zeros([len(time_range),len(Y0)])
+    Y[0]=Y0#<---initial conditions
+    def dYdt(Y,t,_args):
+    	Y[Y<0]=0#this is to make rk work
+    	return np.array(_dYdt(Y,t,*_args))#decorate the function to return an np array
 
-	for i,t in enumerate(time_range[:-1]):
-		h=time_range[i+1]-time_range[i]
-		Y_i=Y[i,:]
-		#Runge-Kutta's terms
-		K_n1=dYdt(Y_i,t,args)
-		K_n2=dYdt(Y_i + (h/2.)*K_n1, t + h/2.,args)
-		K_n3=dYdt(Y_i + (h/2.)*K_n2, t + h/2.,args)
-		K_n4=dYdt(Y_i + h*K_n3, t + h,args)
+    for i,t in enumerate(time_range[:-1]):
+        h=time_range[i+1]-time_range[i]
+        Y_j=Y[i]
+        h_j=h/float(steps)
+        for j in range(0,steps):
+            #Runge-Kutta's terms
+            K_n1=dYdt(Y_j,t,args)
+            K_n2=dYdt(Y_j + (h_j/2.)*K_n1, t + h_j/2.,args)
+            K_n3=dYdt(Y_j + (h_j/2.)*K_n2, t + h_j/2.,args)
+            K_n4=dYdt(Y_j + h_j*K_n3, t + h,args)
 
-		Y[i+1]=Y_i+ (h/6.0)*(K_n1 + 2.0*K_n2 + 2.0*K_n3 + K_n4)
+            Y_j=Y_j+ (h_j/6.0)*(K_n1 + 2.0*K_n2 + 2.0*K_n3 + K_n4)
+            t=t+h_j
+        Y[i+1]=Y_j
 
-	return Y
+    return Y
 
 from scipy.integrate import ode
 def scipy_solve(_dYdt,Y0,time_range,name,kwargs,args=()):
