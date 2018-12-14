3f06e039a45f707b9b1e41d336e44daaad0c770d
exequiel <exequiel.aguirre@gmail.com>
2018-12-14 08:34:28 -0300

    moving Water out of main equations. We solve it in another part and create an interpolator to use in the main equations

--- src/config.py
+++ src/config.py
@@ -37,15 +37,16 @@ class Configuration:
     vBS_d=self.getArray('breeding_site','distribution')
     vBS_s=self.getArray('breeding_site','surface')
     vBS_h=self.getArray('breeding_site','height')
+    vBS_W0=self.getArray('breeding_site','initial_water')
     vBS_mf=self.getArray('breeding_site','manually_filled')
     initial_condition=self.getArray('simulation','initial_condition')
     alpha0=self.getArray('biology','alpha0')
+
     assert np.all(vBS_d>0),'vBS_d cannot have a zero in it'#not allowed anymore
-    assert len(vBS_d) == len(vBS_h) == len(vBS_s) == len(vBS_mf),'vBS_d, vBS_h, vBS_s and vBS_mf must have the same dimension!'
-    n=len(vBS_d)
-    assert len(alpha0)== n, 'dim(alpha0)!=%s'%(n)
+    assert len(vBS_d) == len(vBS_h) == len(vBS_s) == len(vBS_W0) == len(vBS_mf) == len(alpha0),'vBS_d, vBS_h, vBS_s, vBS_W0, vBS_mf and alpha0 must have the same dimension!'
     assert abs(1.-np.sum(vBS_d))<1e-10,'sum(vBS_d)=%s!=1'%(np.sum(vBS_d))
-    assert len(initial_condition)==3*n+1+1+n#(vE+vL+vP) +A1 +A2 +vW
+    n=len(vBS_d)
+    assert len(initial_condition)==3*n+1+1#(vE+vL+vP) +A1 +A2
 
 
   def save(self,filename):
