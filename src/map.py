495645bf3f0b3e05357c418c88a483d71ab5b693
exequiel.aguirre <exequiel.aguirre@gmail.com>
2019-05-04 11:53:14 -0300

    now showing a temporal dimension

--- src/map.py
+++ src/map.py
@@ -12,7 +12,7 @@ import datetime
 
 DATA_FOLDER='data/public/'
 
-def getValue(location):
+def getValues(location):
     #run simulation for location
     mf=0.
     h=10.
@@ -30,7 +30,14 @@ def getValue(location):
     BS_a=model.parameters.BS_a
     O=Y[:,OVIPOSITION]
     lwO=np.sum([Y[indexOf(t),OVIPOSITION]-Y[indexOf(t-7),OVIPOSITION] for t in time_range],axis=1)/BS_a#calculate the difference,sum up all levels, and divide by amount of containers
-    return np.max(lwO)
+    values={}
+    dates=[datetime.date(2018,12,1),datetime.date(2019,1,1),datetime.date(2019,2,1),datetime.date(2019,3,1),datetime.date(2019,4,1)]
+    start_date=configuration.getDate('simulation','start_date')
+    for i in range(len(dates)-1):
+        date,next_date=dates[i],dates[i+1]
+        start,end=(date-start_date).days,(next_date-start_date).days
+        values[date.month]=np.max(lwO[indexOf(start):indexOf(end)])
+    return values
 
 
 def generateCSV():
@@ -39,12 +46,13 @@ def generateCSV():
     config_parser.read('resources/get_weather.cfg')
     params=[]
     start_date,end_date=datetime.date(2015,7,15),datetime.date(2019,5,1)
-    output='name,value,lat,lon\n'
+    output='location,month,value,lat,lon\n'
     for location in config_parser.sections():
         lat=float(config_parser.get(location,'lat'))
         lon=float(config_parser.get(location,'lon'))
-        value=getValue(location)
-        output+='%s, %s, %s, %s\n'%(location,value,lat,lon)
+        values=getValues(location)
+        for month in values:
+            output+='%s, %s, %s, %s, %s\n'%(location,month,values[month],lat,lon)
     filename=DATA_FOLDER+'arg_cities.csv'
     open(filename,'w').write(output)
     return filename
@@ -53,53 +61,75 @@ def plotMap():
     df = pd.read_csv(generateCSV())
     df.head()
 
-    df['text'] = df['name'] + '<br>Max weekly oviposition ' + df['value'].astype(str)
-    limits = [(0,2),(3,10),(11,20),(21,50),(50,3000)]
-    colors = ["rgb(0,116,217)","rgb(255,65,54)","rgb(133,20,75)","rgb(255,133,27)","lightgrey"]
-    cities = []
+    cases = []
+    colors = ['rgb(239,243,255)','rgb(189,215,231)','rgb(107,174,214)','rgb(33,113,181)']
+    months = {12:'Dec',1:'Jan',2:'Feb',3:'Mar'}
 
-
-    for i in range(len(limits)):
-        lim = limits[i]
-        df_sub = df[lim[0]:lim[1]]
-        city = go.Scattergeo(
-            locationmode = 'country names',
-            lon = df_sub['lon'],
-            lat = df_sub['lat'],
-            text = df_sub['text'],
-            marker = go.scattergeo.Marker(
-                size = df_sub['value'],
-                color = colors[i],
-                line = go.scattergeo.marker.Line(
-                    width=0.5, color='rgb(40,40,40)'
-                ),
-                sizemode = 'area'
-            ),
-            name = '{0} - {1}'.format(lim[0],lim[1]) )
-        cities.append(city)
-
-    layout = go.Layout(
-            title = go.layout.Title(
-                text = '2018-2019 Argentina'
-            ),
-            showlegend = True,
-            geo = go.layout.Geo(
-                scope = 'south america',
-                projection = go.layout.geo.Projection(
-                    type='mercator'
-                ),
-                showland = True,
-                landcolor = 'rgb(217, 217, 217)',
-                subunitwidth=1,
-                countrywidth=1,
-                subunitcolor="rgb(255, 255, 255)",
-                countrycolor="rgb(255, 255, 255)"
+    for i in [12,1,2,3][::-1]:
+        cases.append(go.Scattergeo(
+                lon = df[ df['month'] == i ]['lon'], #-(max(range(6,10))-i),
+                lat = df[ df['month'] == i ]['lat'],
+                text = df[ df['month'] == i ]['value'],
+                name = months[i],
+                marker = go.scattergeo.Marker(
+                    size = df[ df['month'] == i ]['value']/25,
+                    color = colors[i%4],
+                    line = go.scattergeo.marker.Line(width = 0)
+                )
             )
         )
 
-    fig = go.Figure(data=cities, layout=layout)
-    ply.plot(fig, filename='d3-bubble-map-populations')
+    cases[0]['text'] = df[ df['month'] == 1 ]['value'].map('{:.0f}'.format).astype(str)+' '+\
+        df[ df['month'] == 1 ]['location']
+    cases[0]['mode'] = 'markers+text'
+    cases[0]['textposition'] = 'bottom center'
 
+    inset = [
+        go.Choropleth(
+            locationmode = 'country names',
+            locations = df[ df['month'] == 1 ]['location'],
+            z = df[ df['month'] == 1 ]['value'],
+            text = df[ df['month'] == 9 ]['location'],
+            colorscale = [[0,'rgb(0, 0, 0)'],[1,'rgb(0, 0, 0)']],
+            autocolorscale = False,
+            showscale = False,
+            geo = 'geo2'
+        ),
+        go.Scattergeo(
+            lon = [21.0936],
+            lat = [7.1881],
+            text = ['Argentina'],
+            mode = 'text',
+            showlegend = False,
+            geo = 'geo2'
+        )
+    ]
+
+    layout = go.Layout(
+        title = go.layout.Title(
+            text = 'Ebola cases reported by month in West Africa 2014<br> \
+    Source: <a href="https://data.hdx.rwlabs.org/dataset/rowca-ebola-cases">\
+    HDX</a>'),
+        geo = go.layout.Geo(
+            resolution = 50,
+            scope = 'south america',
+            showframe = False,
+            showcoastlines = True,
+            showland = True,
+            landcolor = "rgb(229, 229, 229)",
+            countrycolor = "rgb(255, 255, 255)" ,
+            coastlinecolor = "rgb(255, 255, 255)",
+            projection = go.layout.geo.Projection(
+                type = 'mercator'
+            )
+        ),
+        legend = go.layout.Legend(
+               traceorder = 'reversed'
+        )
+    )
+
+    fig = go.Figure(layout=layout, data=cases)
+    ply.plot(fig, filename='2017-2018 maximum weekly oviposition')
 
 if(__name__ == '__main__'):
     plotMap()
