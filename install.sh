904c7fcfa083a0defa130eb417dd05fe9d8eb254
exequiel.aguirre <exequiel.aguirre@gmail.com>
2020-02-24 22:49:57 -0300

    updating install

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo apt install -y python3-dev git python3-gdal python3-skimage python3-pip python3-grib python3-netcdf4
-pip3 install scipy==0.19.1 numpy==1.13.3 matplotlib==2.1.1 similaritymeasures==0.3.0 scikit_image==0.13.1 plotly==2.2.3 folium==0.10.1 Pillow==7.0.0 scikit_learn==0.22.1 moviepy==0.2.3.5 --user
-#cupy==5.1.0 pandas==0.22.0 optionals
+sudo apt update && sudo apt install -y git python3-pip python3-numpy python3-gdal python3-grib python3-netcdf4 ffmpeg
+pip3 install matplotlib==2.1.1 scipy scikit_image folium moviepy similaritymeasures plotly Pillow scikit_learn scikit-image --user
+#cupy==7.1.1 optionals
 
 #clone the project
-git clone ssh://exequiel@pluton.hopto.org:8080/home/exequiel/repositories/aedes_aegypti.git
+git clone http://pluton/repositories/aedes_aegypti.git
 
 #**Installing pybind11**
 git clone https://github.com/pybind/pybind11.git
@@ -15,5 +15,16 @@ git clone https://gitlab.com/libeigen/eigen.git
 sudo cp eigen/Eigen /usr/include/  -r
 
 #**c++ binding**
-cd aedes_aegypti
-g++ -std=c++14 -Wall -O3 -march=native -shared -fPIC -I/usr/include/python3.6m src/cpp/otero_precipitation_wrapper.cpp -o src/otero_precipitation_wrapper.so
+cd aedes_aegypti && g++ -std=c++17 -Wall -O3 -march=native -shared -fPIC -I/usr/include/python3.6m src/cpp/otero_precipitation_wrapper.cpp -o src/otero_precipitation_wrapper.so
+
+#download weather data
+wget  -r -l 1 -A .csv -np -nH -R index.html http://pluton/data/public
+
+#TODO: add line  for the cron
+
+#add private folder
+unzip private.zip -d data/
+
+#to test
+#python src/test 0;python src/test 1;python src/test 6;python src/test fit;python src/test plotFit;python src/test plotFitConf;
+python3 src/tests.py 6
